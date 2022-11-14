0948f509bcc3aacec7256d745344937971992497
Rmanda3 <118176975+Rmanda3@users.noreply.github.com>
2022-11-14 17:28:41 +0530

    creating react project

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import './App.css';
+import HomeView from './Home';
+
+const App:React.FC = ()=> {
+  return (<HomeView/>);
+}
+
+export default App;
---
+++ src/Home/Home.tsx
@@ -0,0 +1,26 @@
+import React from "react"
+import logo from '../logo.svg';
+
+export type Props = {
+
+};
+
+const Home: React.FC<Props> = ({})=>{
+    return (<div className="App">
+    <header className="App-header">
+      <img src={logo} className="App-logo" alt="logo" />
+      <p>
+       Hello World!
+      </p>
+      <a
+        className="App-link"
+        href="https://reactjs.org"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Learn React
+      </a>
+    </header>
+  </div>);
+};
+export default Home;
